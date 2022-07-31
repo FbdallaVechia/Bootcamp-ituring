8ac4d03874e9282317d345b7ba9d3417acfa8903
FbdallaVechia <fabiodallavechia@gmail.com>
2022-07-31 14:51:10 -0300

    Adicionar pratica-1 . 2

--- tarefa-semana-1/pratica-1.js
+++ tarefa-semana-1/pratica-1.js
@@ -3,3 +3,11 @@ let email = "fabiodallavechia@gmail.com";
 let profissao = "Farmacêutico";
 let idade = 33
 let viagemSonhos = "Fazer uma EuroTrip"
+
+console.log(nome, email, profissao, idade, viagemSonhos)
+
+console.log(typeof (nome))
+console.log(typeof (email))
+console.log(typeof (profissao))
+console.log(typeof (idade))
+console.log(typeof (viagemSonhos))
