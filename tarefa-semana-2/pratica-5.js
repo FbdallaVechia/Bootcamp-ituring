12d4b83d4b529310a2234a3c69cb00c7a71bd28d
FbdallaVechia <fabiodallavechia@gmail.com>
2022-08-04 11:40:45 -0300

    Adicionar tarefa-5 semana 2

---
+++ tarefa-semana-2/pratica-5.js
@@ -0,0 +1,9 @@
+
+let valorFinal = function (valorProduto, porcentagemDesconto) {
+    let valorComDesconto = valorProduto - (valorProduto * porcentagemDesconto/100);
+    return valorComDesconto;
+    
+}
+
+let preco = valorFinal (100, 10)
+console.log(preco)
