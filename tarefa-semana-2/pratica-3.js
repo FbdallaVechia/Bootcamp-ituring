34d714ba5ca1370cfd91e3b1b80e01c560dc5475
FbdallaVechia <fabiodallavechia@gmail.com>
2022-08-04 11:16:29 -0300

    Adicionar pratica-3 semana 2

---
+++ tarefa-semana-2/pratica-3.js
@@ -0,0 +1,10 @@
+function numeroAleatorio (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+let numero = numeroAleatorio(0, 1000)
+console.log(numero)
+
