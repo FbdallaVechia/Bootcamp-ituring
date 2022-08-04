3ab0a3d5328a37789875e85ed0347da45def4cd6
FbdallaVechia <fabiodallavechia@gmail.com>
2022-08-04 11:20:45 -0300

    Adicionar tarefa-3 semana 2

--- tarefa-semana-2/pratica-3.js
+++ tarefa-semana-2/pratica-3.js
@@ -8,3 +8,13 @@ function numeroAleatorio (min, max) {
 let numero = numeroAleatorio(0, 1000)
 console.log(numero)
 
+function numeroAleatorioSemParametro () {
+    min = 0;
+    max = 1000;
+
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+let numero1 = numeroAleatorioSemParametro()
+console.log(numero1)
+
