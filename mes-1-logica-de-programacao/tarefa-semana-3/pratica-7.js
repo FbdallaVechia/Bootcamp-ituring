1548e14a359a5a8e47bc7df74bfe533823e20fed
FbdallaVechia <fabiodallavechia@gmail.com>
2022-08-15 18:20:05 -0300

    Adicionar pratica-8 semana 3

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-7.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-7.js
@@ -4,9 +4,9 @@ function saldo (lista) {
     for (let i = 0; i < lista.length; i++){
         soma = soma + lista[i]
     }
+    console.log(`O saldo final de sua conta foi positivo no valor de ${soma}`)
     return soma
 }
 
 let pratica7 = [100, -20, -30, 10, -7, -21, -5]
 let extrato = saldo (pratica7)
-console.log(extrato)
---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-8.js
@@ -0,0 +1,22 @@
+function extrato (lista) {
+    let depositos = 0
+    let saques = 0
+    let saldo = 0
+
+    for (let i= 0; i < lista.length; i++){
+        if (lista[i] > 0) {
+            depositos += lista[i]
+            
+        } else if (lista[i] < 0) {
+            saques += lista[i]
+        }
+    saldo = depositos + saques
+    }
+    console.log (`O valor total depositado foi de: R$ ${depositos}`)
+    console.log (`O valor total sacado foi de: R$ ${saques*-1}`)
+    console.log (`O valor saldo final de sua conta é de: R$ ${saldo}`)
+}
+
+let movimentoConta = [100, -20, -30, 10, -7, -21, -5]
+
+extrato (movimentoConta)
