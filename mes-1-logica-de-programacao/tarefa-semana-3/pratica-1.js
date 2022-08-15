c7bae74cd16e5d0ccb9efcf29a927119d4f9dea4
FbdallaVechia <fabiodallavechia@gmail.com>
2022-08-15 13:46:48 -0300

    Adicionar pratica-1 semana 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-1.js
@@ -0,0 +1,12 @@
+function tabuada (n){
+    let i = 0;
+    let resultado
+    while (i <= 10) {
+        resultado = i * n
+        console.log(`${n} x ${i} = ${resultado}`)
+        i ++
+    }
+    
+}
+
+tabuada (9)
