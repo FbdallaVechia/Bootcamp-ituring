7ea87581f0e265f3cc2424ae1127173cfbc388c7
FbdallaVechia <fabiodallavechia@gmail.com>
2022-08-15 15:44:47 -0300

    Adiconar pratica-3 semana 3

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-2.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-2.js
@@ -1,9 +1,14 @@
 function impares(x) {
+    if (x > 0){
     for (let i = 1; i <= x; i++){
         if (i % 2 != 0){
             console.log(i)
         }
     }
+} else {
+    console.log(`O valor ${x} é inválido `)
+}
 }
 
     impares (17)
+    impares(0)
---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-3.js
@@ -0,0 +1,20 @@
+function primo (y) {
+    let ehPrimo = true
+
+    for (let i = 2; i < y; i++){
+        if (y % i === 0){
+            ehPrimo = false
+        }
+    }
+    console.log(ehPrimo)
+
+}
+
+primo (17)
+primo (2)
+primo (7)
+primo (8)
+primo (5)
+primo (23)
+primo (15)
+primo (1500450271)
