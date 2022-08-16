a20b955995b1c54072bbe09fc89b63ee17800e7f
FbdallaVechia <fabiodallavechia@gmail.com>
2022-08-15 22:39:53 -0300

    Adicionar pratica-12 semana 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-12.js
@@ -0,0 +1,13 @@
+function grupos (alunos) {
+    let qtdeDeGrupos = alunos.length / 2
+
+    alunos.slice(function(grupo){
+
+    })
+}
+
+let turma = ["Anderson Luz", "Fabio Vechia", "Claudia Lopes", "Rafael Alves", "Maria Mansano", "Jairo Ferreira", "Vitor Ribeiro"]
+
+console.log(turma.slice(0,2))
+console.log(turma.slice(2,4))
+console.log(turma.slice(4,7))
