80c6065394677cf0e7c135cf451df840c6b2ae50
FbdallaVechia <fabiodallavechia@gmail.com>
2022-08-15 22:10:16 -0300

    Adicionar pratica-10 semana 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-10.js
@@ -0,0 +1,33 @@
+function votacao(votos) {
+    let brasil = 0
+    let argentina = 0
+    let espanha = 0
+    let portugal = 0
+
+    for (let item of votos) {
+        if (item === 1) {
+            brasil++
+        } else if (item === 2) {
+            argentina++
+        } else if (item === 3) {
+            espanha++
+        } else if (item === 4) {
+            portugal++
+        }
+
+    }
+    console.log(`Brasil: ${brasil} voto(s)`)
+    console.log(`Argentina: ${argentina} voto(s)`)
+    console.log(`Espanha: ${espanha} voto(s)`)
+    console.log(`Portugal: ${portugal} voto(s)`)
+
+    if (brasil > argentina && brasil > espanha && brasil > portugal) {
+        console.log("Brasil é o preferido para ganhar a Copa do Mundo")
+    } 
+
+
+}
+
+let eleicao = [1, 1, 1, 4, 4, 2, 3, 2]
+
+votacao(eleicao)
---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-6.js
@@ -0,0 +1,15 @@
+function buscaTexto (lista, termoDeBusca){
+    let listaFiltrada = []
+    
+    for (let serie of lista) {
+        if (serie.includes(termoDeBusca)) {
+            listaFiltrada.push(serie)
+        }
+    }
+
+    return listaFiltrada
+}
+
+listaSeries = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+
+console.log(buscaTexto(listaSeries, "Th"))
