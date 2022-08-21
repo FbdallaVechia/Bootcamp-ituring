b9a4bd1045bb9aadbbe2c129bd0a26f851cbb5e0
FbdallaVechia <fabiodallavechia@gmail.com>
2022-08-20 23:13:00 -0300

    Adicionar pratica 5 semana 4

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-5.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-5.js
@@ -7,7 +7,7 @@ function buscaSerie (lista, busca) {
         } else {
             console.log(undefined)
         }
-    //lista.includes(busca) ? console.log(lista.indexOf(busca)) : console.log(undefined)
+  //  lista.includes(busca) ? console.log(lista.indexOf(busca)) : console.log(undefined)
 }
 listaSeries = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"] 
 
