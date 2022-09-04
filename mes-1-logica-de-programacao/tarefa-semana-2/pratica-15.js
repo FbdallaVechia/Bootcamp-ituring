e0cc47ec7290b34a19bdf8eb65ab8bab1838d622
FbdallaVechia <fabiodallavechia@gmail.com>
2022-09-04 16:02:23 -0300

    Adicionar resumo semana 1 mes 2

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-15.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-15.js
@@ -1,30 +1,31 @@
 let imc = function (pesoKg, alturaM) {
-    calculoIMC = pesoKg / (alturaM * alturaM); // variável adota escopo global porque não foi declarada dentro da função!!!
+   let calculoIMC = pesoKg / (alturaM * alturaM); // variável adota escopo global porque não foi declarada dentro da função!!!
     console.log(`Seu IMC é de ${calculoIMC.toFixed(2)}`);
 
     return calculoIMC;
 }
 
 function classificacaoIMC(imcCalculado) {
-    if (calculoIMC < 18.5) {
+    if (imcCalculado < 18.5) {
         console.log('Abaixo do peso');
-    } else if (calculoIMC >= 18.5 && calculoIMC < 25) {
+    } else if (imcCalculado >= 18.5 && imcCalculado < 25) {
         console.log('Peso Normal');
-    } else if (calculoIMC >= 25 && calculoIMC < 30) {
+    } else if (imcCalculado >= 25 && imcCalculado < 30) {
         console.log('Sobrepeso');
-    } else if (calculoIMC >= 30) {
+    } else if (imcCalculado >= 30) {
         console.log('Obesidade')
     }
+    return imcCalculado
 }
 
-imc(50, 1.70)
-classificacaoIMC()
 
-imc(70, 1.70)
-classificacaoIMC()
+classificacaoIMC(imc(50, 1.70))
 
-imc(85, 1.7)
-classificacaoIMC()
 
-imc(100, 1, 7)
-classificacaoIMC()
+classificacaoIMC(imc(70, 1.70))
+
+
+classificacaoIMC(imc(85, 1.7))
+
+
+classificacaoIMC(imc(100, 1.7))
